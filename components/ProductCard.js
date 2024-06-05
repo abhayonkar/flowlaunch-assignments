62308b028bbf6ebd9c96a52b4d8ed62e581b6f89
abhayonkar <abhayronkar@gmail.com>
2024-06-05 23:28:47 +0530

    opacity

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,17 +1,32 @@
+const truncateTitle = (title, wordLimit) => {
+  const words = title.split(' ');
+  if (words.length <= wordLimit) {
+    return title;
+  }
+  return words.slice(0, wordLimit).join(' ') + '...';
+};
+
 const ProductCard = ({ product, onClick }) => {
+  const truncatedTitle = truncateTitle(product.title, 4); // Adjust the word limit as needed
+
   return (
     <div
-      className="bg-white dark:bg-gray-700 rounded-lg overflow-hidden shadow-md cursor-pointer flex flex-col items-center justify-center"
+      className="relative bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow-md cursor-pointer transform hover:scale-105 transition-transform duration-300"
       onClick={onClick}
     >
-<div className="w-full h-40 overflow-hidden">
-  <img className="w-full h-auto object-cover" src={product.image} alt={product.title} />
-</div>
-
-
+      <div
+        className="w-full h-40 bg-cover bg-center"
+        style={{ backgroundImage: `url(${product.image})` }}
+      >
+        
+      </div>
       <div className="p-4 text-center">
-        <h2 className="text-xl font-semibold">{product.title}</h2>
-        <p className="text-gray-600">${product.price}</p>
+          <div className=" flex items-center justify-center">
+          <h2 className="text-xl font-semibold text-white p-2 bg-black bg-opacity-50 rounded">
+            {truncatedTitle}
+          </h2>
+        </div>
+        <p className="text-gray-600 dark:text-gray-400">${product.price}</p>
       </div>
     </div>
   );
