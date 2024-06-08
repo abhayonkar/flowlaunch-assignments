e6d54ec0d485c35504e1cc128bf98b61c8982bc1
abhayonkar <abhayronkar@gmail.com>
2024-06-08 23:56:03 +0530

    cards

--- components/ProductDetailsModal.js
+++ components/ProductDetailsModal.js
@@ -17,9 +17,11 @@ const ProductDetailsModal = ({ product, onClose }) => {
 
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-gray-500 bg-opacity-50 modal-overlay">
-      <div className="bg-white dark:bg-gray-900 rounded-lg overflow-hidden shadow-md relative flex flex-col md:flex-row max-w-3xl w-full">
-        <img className="w-full md:w-1/2 h-64 object-contain" src={product.image} alt={product.title} />
-        <div className="p-4 md:w-1/2">
+      <div className="bg-white dark:bg-gray-900 rounded-lg overflow-hidden shadow-md relative flex flex-col md:flex-row max-w-3xl w-full m-4 md:m-8">
+        <div className="flex-shrink-0">
+          <img className="w-full md:w-96 h-64 object-contain p-4" src={product.image} alt={product.title} />
+        </div>
+        <div className="p-4 md:w-1/2 flex flex-col justify-center">
           <h2 className="text-xl font-semibold mb-2 text-gray-900 dark:text-gray-100">{product.title}</h2>
           <p className="text-gray-600 mb-2 dark:text-gray-400">${product.price}</p>
           <p className="text-gray-800 dark:text-gray-300">{product.description}</p>
